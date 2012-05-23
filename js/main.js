67e3bb42c5caea663c2faa8b9d6a72b7515ddf60
xaviuzz <xaviuzz@prometeuzzzz.(none)>
2012-05-23 16:53:39 +0200

    adjustments by xav1uzz

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-/* Author:
-
-*/
-
-
-
-
-
+CUORE.Dom.ready(function() {
+    // cache for labelService
+    var currentLocale = (navigator.language || navigator.browserLanguage);
+    document.labels = {};
+    document.labels[currentLocale] = {
+        
+       // "a.key": "Text ",
+        
+    };
+    
+    CUORE.Bus.enableDebug();
+    
+    document.page = new CUORE.Page("http://www.anydomain.com");
+    document.page.draw();
+});
